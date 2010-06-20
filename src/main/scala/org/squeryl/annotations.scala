b908d1c267ce92563d800402752f8956a85137d2
Maxime Lévesque <maxime.levesque@gmail.com>
2010-06-20 09:49:41 -0400

    doc for org.squeryl.annotations.Column

--- src/main/scala/org/squeryl/annotations.scala
+++ src/main/scala/org/squeryl/annotations.scala
@@ -19,5 +19,25 @@ import annotation.target.field
 import annotations.ColumnBase
 
 package object annotations {
+  /**
+   * The preferred way to define column metadata is not not define them (!)
+   * Squeryl has default mappings for all Java primitive types.
+   * Scala/Java
+   * Int/int  -> 4 byte number
+   * Long/long  -> 8 byte number
+   * Float/float -> 4 byte floating point
+   * String -> varchar(256)
+   *
+   * The default mappings can be overriden at the field/column level using the
+   * Column attribute, and they can also be  overriden at the Schema level
+   * by overriding the method. For example, the following causes all string
+   * field in the schema to become varchars of length 64 :
+   *
+   * override def columnTypeFor(fieldMetaData: FieldMetaData, databaseAdapter: DatabaseAdapter) =
+   *   if(fieldMetaData.isStringType)
+   *     return "varchar(64)"
+   *   else
+   *     super.columnTypeFor(fieldMetaData, databaseAdapter)
+   */
   type Column = ColumnBase @field
 }
--- src/main/scala/org/squeryl/annotations/ColumnBase.java
+++ src/main/scala/org/squeryl/annotations/ColumnBase.java
@@ -19,25 +19,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * The preferred way to define column metadata is not not define them (!)
- * Squeryl has default mappings for all Java primitive types.
- * Scala/Java
- * Int/int  -> 4 byte number
- * Long/long  -> 8 byte number
- * Float/float -> 4 byte floating point
- * String -> varchar(256) 
- *
- * The default mappings can be overriden at the field/column level using the
- * Column attribute, and they can also be  overriden at the Schema level
- * by overriding the method. For example, the following causes all string
- * field in the schema to become varchars of length 64 :
- *   
- * override def columnTypeFor(fieldMetaData: FieldMetaData, databaseAdapter: DatabaseAdapter) =
- *   if(fieldMetaData.isStringType)
- *     return "varchar(64)"
- *   else
- *     super.columnTypeFor(fieldMetaData, databaseAdapter)
- *
+ * This annotation should not be used directly, but via it's subtype : 
+ * org.squeryl.annotations.Column
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ColumnBase {
