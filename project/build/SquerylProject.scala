acb7aa16569a6ea67c1e0196cb006efe9819cc83
Maxime Lévesque <maxime.levesque@gmail.com>
2010-06-20 23:27:31 -0400

    specify snapshot repo for Specs

--- project/build/SquerylProject.scala
+++ project/build/SquerylProject.scala
@@ -35,5 +35,7 @@ class SquerylProject(info: ProjectInfo) extends DefaultProject(info) {
 	
   val posgresDriver = "postgresql" % "postgresql" % "8.4-701.jdbc4" % "provided"
 
-  val specs = "org.scala-tools.testing" % "specs_2.8.0.Beta1" % "1.6.4" % "test"
+  val snapshotsRepo = Resolver.url("snapshots-repo", new java.net.URL("http://www.scala-tools.org/repo-snapshots"))
+
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT"
 }
