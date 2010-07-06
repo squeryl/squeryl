e5c2256cd4eaba5ac18f4a68a35bae501eee0cb5
Maxime Lévesque <maxime.levesque@gmail.com>
2010-07-05 22:54:19 -0400

    support for joining up to 10 table/object (was 7)

--- src/main/scala/org/squeryl/dsl/boilerplate/FromSignatures.scala
+++ src/main/scala/org/squeryl/dsl/boilerplate/FromSignatures.scala
@@ -64,4 +64,43 @@ trait FromSignatures {
           t7:Queryable[T7]
           )(f:Function7[T1,T2,T3,T4,T5,T6,T7,QueryYield[R]]): Query[R] =
     new Query7[T1,T2,T3,T4,T5,T6,T7,R](t1,t2,t3,t4,t5,t6,t7,f,true)
+
+  def from[T1,T2,T3,T4,T5,T6,T7,T8,R](
+          t1:Queryable[T1],
+          t2:Queryable[T2],
+          t3:Queryable[T3],
+          t4:Queryable[T4],
+          t5:Queryable[T5],
+          t6:Queryable[T6],
+          t7:Queryable[T7],
+          t8:Queryable[T8]
+          )(f:Function8[T1,T2,T3,T4,T5,T6,T7,T8,QueryYield[R]]): Query[R] =
+    new Query8[T1,T2,T3,T4,T5,T6,T7,T8,R](t1,t2,t3,t4,t5,t6,t7,t8,f,true)
+
+  def from[T1,T2,T3,T4,T5,T6,T7,T8,T9,R](
+          t1:Queryable[T1],
+          t2:Queryable[T2],
+          t3:Queryable[T3],
+          t4:Queryable[T4],
+          t5:Queryable[T5],
+          t6:Queryable[T6],
+          t7:Queryable[T7],
+          t8:Queryable[T8],
+          t9:Queryable[T9]
+          )(f:Function9[T1,T2,T3,T4,T5,T6,T7,T8,T9,QueryYield[R]]): Query[R] =
+    new Query9[T1,T2,T3,T4,T5,T6,T7,T8,T9,R](t1,t2,t3,t4,t5,t6,t7,t8,t9,f,true)
+
+  def from[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,R](
+          t1:Queryable[T1],
+          t2:Queryable[T2],
+          t3:Queryable[T3],
+          t4:Queryable[T4],
+          t5:Queryable[T5],
+          t6:Queryable[T6],
+          t7:Queryable[T7],
+          t8:Queryable[T8],
+          t9:Queryable[T9],
+          t10:Queryable[T10]
+          )(f:Function10[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,QueryYield[R]]): Query[R] =
+    new Query10[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,R](t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,f,true)
 }
--- src/main/scala/org/squeryl/dsl/boilerplate/Query1.scala
+++ src/main/scala/org/squeryl/dsl/boilerplate/Query1.scala
@@ -196,3 +196,129 @@ class Query7[T1,T2,T3,T4,T5,T6,T7,R](
     sq1,sq2,sq3,sq4,sq5,sq6,sq7
   )
 }
+
+class Query8[T1,T2,T3,T4,T5,T6,T7,T8,R](
+  t1:Queryable[T1],
+  t2:Queryable[T2],
+  t3:Queryable[T3],
+  t4:Queryable[T4],
+  t5:Queryable[T5],
+  t6:Queryable[T6],
+  t7:Queryable[T7],
+  t8:Queryable[T8],
+  f: Function8[T1,T2,T3,T4,T5,T6,T7,T8,QueryYield[R]], isRoot:Boolean) extends AbstractQuery[R](isRoot) {
+
+  val sq1 = createSubQueryable(t1)
+  val sq2 = createSubQueryable(t2)
+  val sq3 = createSubQueryable(t3)
+  val sq4 = createSubQueryable(t4)
+  val sq5 = createSubQueryable(t5)
+  val sq6 = createSubQueryable(t6)
+  val sq7 = createSubQueryable(t7)
+  val sq8 = createSubQueryable(t8)
+
+  def createCopy(root:Boolean) = new Query8[T1,T2,T3,T4,T5,T6,T7,T8,R](t1,t2,t3,t4,t5,t6,t7,t8,f,root)
+
+  def invokeYield(rsm: ResultSetMapper, rs: ResultSet): R =
+    f(sq1.give(rs),
+      sq2.give(rs),
+      sq3.give(rs),
+      sq4.give(rs),
+      sq5.give(rs),
+      sq6.give(rs),
+      sq7.give(rs),
+      sq8.give(rs)
+    ).invokeYield(rsm, rs)
+
+  val ast = buildAst(
+    f(sq1.sample, sq2.sample, sq3.sample, sq4.sample, sq5.sample, sq6.sample, sq7.sample, sq8.sample),
+    sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8
+  )
+}
+
+class Query9[T1,T2,T3,T4,T5,T6,T7,T8,T9,R](
+  t1:Queryable[T1],
+  t2:Queryable[T2],
+  t3:Queryable[T3],
+  t4:Queryable[T4],
+  t5:Queryable[T5],
+  t6:Queryable[T6],
+  t7:Queryable[T7],
+  t8:Queryable[T8],
+  t9:Queryable[T9],
+  f: Function9[T1,T2,T3,T4,T5,T6,T7,T8,T9,QueryYield[R]], isRoot:Boolean) extends AbstractQuery[R](isRoot) {
+
+  val sq1 = createSubQueryable(t1)
+  val sq2 = createSubQueryable(t2)
+  val sq3 = createSubQueryable(t3)
+  val sq4 = createSubQueryable(t4)
+  val sq5 = createSubQueryable(t5)
+  val sq6 = createSubQueryable(t6)
+  val sq7 = createSubQueryable(t7)
+  val sq8 = createSubQueryable(t8)
+  val sq9 = createSubQueryable(t9)
+
+  def createCopy(root:Boolean) = new Query9[T1,T2,T3,T4,T5,T6,T7,T8,T9,R](t1,t2,t3,t4,t5,t6,t7,t8,t9,f,root)
+
+  def invokeYield(rsm: ResultSetMapper, rs: ResultSet): R =
+    f(sq1.give(rs),
+      sq2.give(rs),
+      sq3.give(rs),
+      sq4.give(rs),
+      sq5.give(rs),
+      sq6.give(rs),
+      sq7.give(rs),
+      sq8.give(rs),
+      sq9.give(rs)
+    ).invokeYield(rsm, rs)
+
+  val ast = buildAst(
+    f(sq1.sample, sq2.sample, sq3.sample, sq4.sample, sq5.sample, sq6.sample, sq7.sample, sq8.sample, sq9.sample),
+    sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8,sq9
+  )
+}
+
+class Query10[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,R](
+  t1:Queryable[T1],
+  t2:Queryable[T2],
+  t3:Queryable[T3],
+  t4:Queryable[T4],
+  t5:Queryable[T5],
+  t6:Queryable[T6],
+  t7:Queryable[T7],
+  t8:Queryable[T8],
+  t9:Queryable[T9],
+  t10:Queryable[T10],
+  f: Function10[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,QueryYield[R]], isRoot:Boolean) extends AbstractQuery[R](isRoot) {
+
+  val sq1 = createSubQueryable(t1)
+  val sq2 = createSubQueryable(t2)
+  val sq3 = createSubQueryable(t3)
+  val sq4 = createSubQueryable(t4)
+  val sq5 = createSubQueryable(t5)
+  val sq6 = createSubQueryable(t6)
+  val sq7 = createSubQueryable(t7)
+  val sq8 = createSubQueryable(t8)
+  val sq9 = createSubQueryable(t9)
+  val sq10 = createSubQueryable(t10)
+
+  def createCopy(root:Boolean) = new Query10[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,R](t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,f,root)
+
+  def invokeYield(rsm: ResultSetMapper, rs: ResultSet): R =
+    f(sq1.give(rs),
+      sq2.give(rs),
+      sq3.give(rs),
+      sq4.give(rs),
+      sq5.give(rs),
+      sq6.give(rs),
+      sq7.give(rs),
+      sq8.give(rs),
+      sq9.give(rs),
+      sq10.give(rs)
+    ).invokeYield(rsm, rs)
+
+  val ast = buildAst(
+    f(sq1.sample, sq2.sample, sq3.sample, sq4.sample, sq5.sample, sq6.sample, sq7.sample, sq8.sample, sq9.sample, sq10.sample),
+    sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8,sq9, sq10
+  )
+}
