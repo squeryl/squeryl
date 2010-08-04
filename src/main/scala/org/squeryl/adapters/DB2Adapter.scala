d49d77d00d8fc289f563ade0deb3fb7c9f1fc54b
cmaslen <christian.maslen@gmail.com>
2010-08-04 23:06:25 +1000

    Added DB2Adapter class and db2 jdbc driver. IBM don't allow distribution of their jars via maven repositories

---
+++ src/main/scala/org/squeryl/adapters/DB2Adapter.scala
@@ -0,0 +1,118 @@
+package org.squeryl.adapters
+
+import org.squeryl.internals.{StatementWriter, DatabaseAdapter}
+import org.squeryl.{Session, Table}
+import java.sql.SQLException
+import org.squeryl.dsl.ast._
+
+class DB2Adapter extends DatabaseAdapter {
+
+  override def booleanTypeDeclaration = "char(1)"
+
+  override def timestampTypeDeclaration = "timestamp"
+
+  override def supportsAutoIncrementInColumnDeclaration: Boolean = false
+
+  override def postCreateTable(s: Session, t: Table[_]) = {
+
+    val sw = new StatementWriter(false, this)
+    sw.write("create sequence ", sequenceName(t), " start with 1 increment by 1 nomaxvalue")
+    val st = s.connection.createStatement
+    st.execute(sw.statement)
+  }
+
+  override def postDropTable(t: Table[_]) =
+    execFailSafeExecute("drop sequence " + sequenceName(t), e => e.getErrorCode == -204)
+
+  def sequenceName(t: Table[_]) = "s_" + t.name
+
+  override def writeInsert[T](o: T, t: Table[T], sw: StatementWriter): Unit = {
+
+    val o_ = o.asInstanceOf[AnyRef]
+
+    val autoIncPK = t.posoMetaData.fieldsMetaData.find(fmd => fmd.isAutoIncremented)
+
+    if (autoIncPK == None) {
+      super.writeInsert(o, t, sw)
+      return
+    }
+
+    val f = t.posoMetaData.fieldsMetaData.filter(fmd => fmd != autoIncPK.get)
+
+    val colNames = List(autoIncPK.get) ::: f.toList
+    val colVals = List("next value for " + sequenceName(t)) ::: f.map(fmd => writeValue(o_, fmd, sw)).toList
+
+    sw.write("insert into ");
+    sw.write(t.name);
+    sw.write(" (");
+    sw.write(colNames.map(fmd => fmd.columnName).mkString(", "));
+    sw.write(") values ");
+    sw.write(colVals.mkString("(", ",", ")"));
+  }
+
+  override def writeConcatFunctionCall(fn: FunctionNode[_], sw: StatementWriter) =
+    sw.writeNodesWithSeparator(fn.args, " || ", false)
+
+  override def isTableDoesNotExistException(e: SQLException) = {
+    e.getErrorCode == -204
+  }
+
+  override def writePaginatedQueryDeclaration(qen: QueryExpressionElements, sw: StatementWriter) = {}
+
+  override def writeQuery(qen: QueryExpressionElements, sw: StatementWriter) =
+    if (qen.page == None)
+      super.writeQuery(qen, sw)
+    else {
+      sw.write("select sq____1.* from (")
+      sw.nextLine
+      sw.writeIndented {
+        sw.write("select sq____0.*, row_number() over() as rn____")
+        sw.nextLine
+        sw.write("from")
+        sw.nextLine
+        sw.writeIndented {
+          sw.write("(")
+          super.writeQuery(qen, sw)
+          sw.write(") sq____0")
+        }
+      }
+      sw.nextLine
+      sw.write(") sq____1")
+      sw.nextLine
+      sw.write("where")
+      sw.nextLine
+      sw.writeIndented {
+        sw.write("rn____ between ")
+        val page = qen.page.get
+        val beginOffset = page._1 + 1
+        val endOffset = page._2 + beginOffset - 1
+        sw.write(beginOffset.toString)
+        sw.write(" and ")
+        sw.write(endOffset.toString)
+      }
+    }
+
+  override def writeConcatOperator[A1, A2](left: TypedExpressionNode[A1], right: TypedExpressionNode[A2], sw: StatementWriter) = {
+    sw.write("(")
+    _writeConcatOperand(left, sw)
+    sw.write(" ")
+    sw.write("||")
+    sw.write(" ")
+    _writeConcatOperand(right, sw)
+    sw.write(")")
+  }
+
+  private def _writeConcatOperand[A1](e: TypedExpressionNode[A1], sw: StatementWriter) = {
+    if (e.isInstanceOf[ConstantExpressionNode[A1]]) {
+      val c = e.asInstanceOf[ConstantExpressionNode[A1]]
+      sw.write("cast(")
+      e.write(sw)
+      sw.write(" as varchar(")
+      sw.write(c.value.toString.length.toString)
+      sw.write("))")
+    }
+    else
+      e.write(sw)
+  }
+
+}
