b31d568765f110a6b1d17261bd2fb2128d83e204
mricog <m.ricogomez@gmail.com>
2011-05-15 22:56:24 +0200

    added derby tests

---
+++ src/test/scala/org/squeryl/derby/DerbyTests.scala
@@ -0,0 +1,45 @@
+package org.squeryl.derby
+
+import org.squeryl.test._
+
+import org.squeryl.framework.DBConnector
+import org.squeryl.adapters.DerbyAdapter
+
+import org.squeryl.Session
+
+trait Derby_Connection extends DBConnector{
+  def connectToDb() : Option[() => Session] = {
+    if(config.hasProps("derby.connectionString", "derby.user", "derby.password")){
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver")
+      Some(() => {
+        val c = java.sql.DriverManager.getConnection(
+          config.getProp("derby.connectionString"),
+          config.getProp("derby.user"),
+          config.getProp("derby.password")
+        )
+        c.setAutoCommit(false)
+        Session.create(c, new DerbyAdapter)
+      })
+    }else{
+      None
+    }
+  }
+}
+
+class Derby_UuidTests extends UuidTests with Derby_Connection
+class Derby_NestedLeftOuterJoinTest extends NestedLeftOuterJoinTest with Derby_Connection
+class Derby_SchoolDbMutableRelations extends mutablerelations.SchoolDb2MetableRelations with Derby_Connection
+class Derby_TransactionTests extends TransactionTests with Derby_Connection
+class Derby_SchoolDb2 extends schooldb2.SchoolDb2Tests with Derby_Connection
+class Derby_SchoolDb extends schooldb.SchoolDbTestRun with Derby_Connection
+class Derby_TestCustomTypesMode extends customtypes.TestCustomTypesMode with Derby_Connection
+class Derby_KickTheTires extends demo.KickTheTires with Derby_Connection
+class Derby_MusicDb extends musicdb.MusicDbTestRun with Derby_Connection
+class Derby_LeftJoinTest extends LeftJoinTest with Derby_Connection
+
+// TODO: What's that test for?
+// class Derby_ConnectionClosing extends ConnectionClosingTest("values CURRENT_TIMESTAMP") with Derby_Connection
+
+
+
+
