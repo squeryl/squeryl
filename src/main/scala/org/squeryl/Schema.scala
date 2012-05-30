ed8be31ab5cf00ebefa6e9ab8813189889d53b78
Maxime Levesque <maxime.levesque@gmail.com>
2012-05-30 08:38:30 -0400

    added a public accessor for all tables of a Schema

--- src/main/scala/org/squeryl/Schema.scala
+++ src/main/scala/org/squeryl/Schema.scala
@@ -32,6 +32,8 @@ trait Schema {
    */
   private val _tables = new ArrayBuffer[Table[_]] 
   
+  def tables: Seq[Table[_]] = _tables.toSeq
+  
   private val _tableTypes = new HashMap[Class[_], Table[_]]
 
   private val _oneToManyRelations = new ArrayBuffer[OneToManyRelation[_,_]]
