76ea410bb58b8be92218397fcd58341fd205b8c6
Boris Chazalet <boris.chazalet@gmail.com>
2020-12-31 18:08:01 +0100

    Fix temporarly the use of a 'protected' variable

--- src/main/scala/org/squeryl/dsl/fsm/BaseQueryYield.scala
+++ src/main/scala/org/squeryl/dsl/fsm/BaseQueryYield.scala
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2010 Maxime Lévesque
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -58,7 +58,9 @@ class BaseQueryYield[G]
   def unevaluatedHavingClause = _havingClause
 
   //TODO: an array is probably more efficient, even if less 'lazy' :
-  protected var _orderByExpressions: () => List[()=>ExpressionNode] = null
+  // protected var _orderByExpressions: () => List[()=>ExpressionNode] = null
+  // FIXME this seemed to have changed in scala 3.0.0? It used to work with protected
+  var _orderByExpressions: () => List[()=>ExpressionNode] = null
 
   def whereClause: Option[ExpressionNode] =
     queryElementzz.whereClause.map(b=>b())
