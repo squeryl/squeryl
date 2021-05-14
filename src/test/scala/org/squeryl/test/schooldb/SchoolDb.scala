604759998a0f263fe88a6e86e299725c9a478b2f
Boris Chazalet <boris.chazalet@gmail.com>
2021-05-14 22:42:24 +0200

    Fix indentation related issue in test

--- src/test/scala/org/squeryl/test/schooldb/SchoolDb.scala
+++ src/test/scala/org/squeryl/test/schooldb/SchoolDb.scala
@@ -866,7 +866,7 @@ abstract class SchoolDbTestRun extends SchoolDbTestBase {
         .select(s.id)
       )
     
-    val z2 =
+    val z2 = {
       from(students)(s=>
         where({
           val a = s.isMultilingual.isNull
@@ -874,6 +874,7 @@ abstract class SchoolDbTestRun extends SchoolDbTestBase {
           })
         .select(s.id)
       )
+    }
                 
       val r1 = z1.toSet
       val r2 = z2.toSet
