aae72336581689d33a5c8e68cac6659adcf4a0c0
xuwei-k <6b656e6a69@gmail.com>
2023-02-18 16:38:32 +0900

    fix package

--- src/main/scala-2/org/squeryl/internals/TableDefinitionInSchema.scala
+++ src/main/scala-2/org/squeryl/internals/TableDefinitionInSchema.scala
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************** */
-package org.squeryl
+package org.squeryl.internals
 
+import org.squeryl._
 import reflect.ClassTag
 
 trait TableDefinitionInSchema {
--- src/main/scala-3/org/squeryl/internals/TableDefinitionInSchema.scala
+++ src/main/scala-3/org/squeryl/internals/TableDefinitionInSchema.scala
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************** */
-package org.squeryl
+package org.squeryl.internals
 
-import internals._
+import org.squeryl._
 import reflect.ClassTag
 
 trait TableDefinitionInSchema {
