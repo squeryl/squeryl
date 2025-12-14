b1cd0e4f406e0c19ae820c9c0b23cfdfd8e52dd6
xuwei-k <6b656e6a69@gmail.com>
2025-12-14 13:40:13 +0900

    separate AppSpecificTypeMode file

---
+++ src/test/scala/org/squeryl/test/schooldb/AppSpecificTypeMode.scala
@@ -0,0 +1,38 @@
+package org.squeryl.test.schooldb
+
+import org.squeryl.dsl._
+import org.squeryl._
+
+object AppSpecificTypeMode extends org.squeryl.PrimitiveTypeMode {
+  implicit object personKED extends KeyedEntityDef[Student, Int] {
+    def getId(a: Student) = a.id
+    def isPersisted(a: Student) = a.id > 0
+    def idPropertyName = "id"
+  }
+
+  implicit object schoolDbObjectKED extends KeyedEntityDef[SchoolDbObject, Int] {
+    def getId(a: SchoolDbObject) = a.id
+    def isPersisted(a: SchoolDbObject) = a.id > 0
+    def idPropertyName = "id"
+  }
+
+  implicit object courseKED extends KeyedEntityDef[Course, Int] {
+    def getId(a: Course) = a.id
+    def isPersisted(a: Course) = a.id > 0
+    def idPropertyName = "id"
+    override def optimisticCounterPropertyName: Option[String] = Some("occVersionNumber")
+  }
+
+  implicit object course2KED extends KeyedEntityDef[Course2, Int] {
+    def getId(a: Course2) = a.id
+    def isPersisted(a: Course2) = a.id > 0
+    def idPropertyName = "id"
+    override def optimisticCounterPropertyName: Option[String] = Some("occVersionNumber")
+  }
+
+  implicit object courseOfferingKED extends KeyedEntityDef[CourseOffering, CompositeKey3[Int, Long, Int]] {
+    def getId(a: CourseOffering) = a.id
+    def isPersisted(a: CourseOffering) = a.isPersisted
+    def idPropertyName = "id"
+  }
+}
--- src/test/scala/org/squeryl/test/schooldb/SchoolDb.scala
+++ src/test/scala/org/squeryl/test/schooldb/SchoolDb.scala
@@ -26,42 +26,7 @@ import adapters.{MSSQLServer, OracleAdapter, DerbyAdapter}
 import internals.{FieldMetaData, FieldReferenceLinker, LifecycleEvent}
 import collection.mutable.ArrayBuffer
 import org.squeryl.dsl.ast.ExpressionNode
-
-object AppSpecificTypeMode extends org.squeryl.PrimitiveTypeMode {
-  implicit object personKED extends KeyedEntityDef[Student, Int] {
-    def getId(a: Student) = a.id
-    def isPersisted(a: Student) = a.id > 0
-    def idPropertyName = "id"
-  }
-
-  implicit object schoolDbObjectKED extends KeyedEntityDef[SchoolDbObject, Int] {
-    def getId(a: SchoolDbObject) = a.id
-    def isPersisted(a: SchoolDbObject) = a.id > 0
-    def idPropertyName = "id"
-  }
-
-  implicit object courseKED extends KeyedEntityDef[Course, Int] {
-    def getId(a: Course) = a.id
-    def isPersisted(a: Course) = a.id > 0
-    def idPropertyName = "id"
-    override def optimisticCounterPropertyName: Option[String] = Some("occVersionNumber")
-  }
-
-  implicit object course2KED extends KeyedEntityDef[Course2, Int] {
-    def getId(a: Course2) = a.id
-    def isPersisted(a: Course2) = a.id > 0
-    def idPropertyName = "id"
-    override def optimisticCounterPropertyName: Option[String] = Some("occVersionNumber")
-  }
-
-  implicit object courseOfferingKED extends KeyedEntityDef[CourseOffering, CompositeKey3[Int, Long, Int]] {
-    def getId(a: CourseOffering) = a.id
-    def isPersisted(a: CourseOffering) = a.isPersisted
-    def idPropertyName = "id"
-  }
-}
-
-import AppSpecificTypeMode._
+import org.squeryl.test.schooldb.AppSpecificTypeMode._
 
 object SingleTestRun extends org.scalatest.Tag("SingleTestRun")
 
